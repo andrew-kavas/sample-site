5dabadbf180d77f243a6c5e264a941f9e478c6eb
“andrew-kavas” <ask410ventures@gmail.com>
2020-10-07 12:42:50 -0500

    added text and json routes & files

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT|| 4000;
 const app = express();
 
-// app.use(express.json());
-
 
 // home
 app.get("/", (req, res) => {
-  res.send("Example site")
+  res.send("Example site 2");
+})
+
+// example text
+app.get("/text", (req, res) => {
+  res.sendFile(path.join(__dirname + "/pages/text.html"));
+})
+
+// example json
+app.get("/json", (req, res) => {
+  let rawdata = fs.readFileSync(__dirname + "/pages/example.json")
+  let example_json = JSON.parse(rawdata)
+  res.send(example_json);
 })
 
 // status info
